d963a766c2197a6b643d2e277d59021b8574a8ce
wngtk <100271394+wngtk@users.noreply.github.com>
2023-11-21 10:03:52 +0800

    add ui

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(requestLogger)
 
@@ -95,7 +96,7 @@ app.delete('/api/notes/:id', (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log('Server running on port', PORT)
 })
