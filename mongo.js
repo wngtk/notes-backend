e63bb1f10bffe72741cd139e9ae507c1e7021898
wngtk <100271394+wngtk@users.noreply.github.com>
2023-11-22 14:32:54 +0800

    add mongo.js for test mongodb connection

---
+++ mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('Please provide the password as an argument: node mongo.js <password>')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const URL = `mongodb+srv://wngtk:${password}@fullstack.1ptpmv8.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.connect(URL)
+
+const noteSchema = new mongoose.Schema({
+    content: String,
+    date: Date,
+    important: Boolean
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//     content: 'Browser can execute only Javascript',
+//     date: new Date(),
+//     important: true
+// })
+
+// note.save().then(result => {
+//     console.log(result);
+//     console.log('note saved!');
+//     mongoose.connection.close();
+// });
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+        console.log(note);
+    })
+    mongoose.connection.close();
+})
