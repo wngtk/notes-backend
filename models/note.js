23f85899570ad75c76aaf85b79122e8dff623f4d
wngtk <100271394+wngtk@users.noreply.github.com>
2023-11-24 22:14:59 +0800

    Fix lint error

--- models/note.js
+++ models/note.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
+const process = require('process')
 
 const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url)
-    .then(result => {
+    .then(() => {
         console.log('connected to MongoDB')
     })
     .catch(error => {
