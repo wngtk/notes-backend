4493b1b6ec40227cdebb494e5df45de8e253fbc1
wngtk <100271394+wngtk@users.noreply.github.com>
2023-11-24 22:13:32 +0800

    Fix lint error

--- mongo.js
+++ mongo.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const process = require('process')
 
 if (process.argv.length < 3) {
     console.log('Please provide the password as an argument: node mongo.js <password>')
@@ -33,7 +34,7 @@ const Note = mongoose.model('Note', noteSchema)
 
 Note.find({}).then(result => {
     result.forEach(note => {
-        console.log(note);
+        console.log(note)
     })
-    mongoose.connection.close();
+    mongoose.connection.close()
 })
