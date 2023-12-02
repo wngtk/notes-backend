81aee6e52cec2d0a481fbca06aa01cf15b06b7e5
wngtk <100271394+wngtk@users.noreply.github.com>
2023-12-02 12:50:42 +0800

    Restructure folders

--- models/note.js
+++ models/note.js
@@ -1,37 +1,24 @@
 const mongoose = require('mongoose')
-const process = require('process')
-
-const url = process.env.MONGODB_URI
-
-console.log('connecting to', url)
-
-mongoose.connect(url)
-    .then(() => {
-        console.log('connected to MongoDB')
-    })
-    .catch(error => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
 
 const noteSchema = new mongoose.Schema({
-    content: {
-        type: String,
-        minLength: 5,
-        required: true
-    },
-    date: {
-        type: Date,
-        required: true
-    },
-    important: Boolean,
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  important: Boolean,
 })
 
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject.__v
-        delete returnedObject._id
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
-module.exports = new mongoose.model('Note', noteSchema)
+module.exports = mongoose.model('Note', noteSchema)
