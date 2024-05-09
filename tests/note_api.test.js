91a957702f86d008e3c7f16fa48a32c08a1ed19b
Wang Tiaoke <fnmain.rs@gmail.com>
2024-05-09 17:46:47 +0800

    Added test: a note can be deleted

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -99,6 +99,20 @@ test('a specific note can be viewed', async () => {
     assert.deepStrictEqual(resultNote.body, noteToView)
 })
 
+test('a note can be deleted', async () => {
+    const notesAtStart = await helper.notesInDb()
+    const noteToDelete = notesAtStart[0]
+    await api
+        .delete(`/api/notes/${noteToDelete.id}`)
+        .expect(204)
+    
+    const notesAtEnd = await helper.notesInDb()
+
+    const contents = notesAtEnd.map(r => r.content)
+    assert(!contents.includes(noteToDelete.content))
+    assert.strictEqual(notesAtEnd.length, helper.initialNotes.length - 1)
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
